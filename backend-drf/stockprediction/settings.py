2fb1d72df16aa6f201c8d953985ee5a97afe6f69
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-08-30 20:19:38 +0600

    Frontend Backend connection Register

--- backend-drf/stockprediction/settings.py
+++ backend-drf/stockprediction/settings.py
@@ -36,9 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
                 <div className='p-5 text-center  rounded'>
                     <h1 className='text-light'>Stock Prediction Portal</h1>
                     <p className='text-light lead'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam fugit perferendis, libero tempora voluptatem quasi maxime, magnam iusto eveniet saepe sunt debitis quia commodi voluptatibus nam id. Exercitationem, laborum recusandae?</p>
-                    <Button text="Login" class="btn-outline-info" />
+                    <Button text="Login" class="btn-outline-info" url="/login" />
                 </div>
             </div>
 
