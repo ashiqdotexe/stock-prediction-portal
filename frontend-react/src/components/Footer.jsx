eb1d018e4cfc98086e0428a606c7942c7275c04e
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-08-30 10:28:16 +0600

    Header Main Footerr

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className='footer py-3 my-3'></footer>
+            <hr className='border-bottom' />
+            <p className='text-light text-center'>&copy; 2025 - Built by <a href="https://www.linkedin.com/in/hmashiqurrahman/" class="link-secondary text-decoration-none">HM Ashiqur Rahman</a></p>
+
+        </>
+    )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <>
+            <div className='container'>
+                <div className='p-5 text-center  rounded'>
+                    <h1 className='text-light'>Stock Prediction Portal</h1>
+                    <p className='text-light lead'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam fugit perferendis, libero tempora voluptatem quasi maxime, magnam iusto eveniet saepe sunt debitis quia commodi voluptatibus nam id. Exercitationem, laborum recusandae?</p>
+                    <a className='btn btn-outline-info' href="">Login</a>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Main
