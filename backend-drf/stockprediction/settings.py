b3444c1072a3cecf3a38dd587c522d7ea44fda8f
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-08-30 14:12:44 +0600

    backend register api

--- backend-drf/stockprediction/settings.py
+++ backend-drf/stockprediction/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -72,8 +73,12 @@ WSGI_APPLICATION = 'stockprediction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stockprediction',          
+        'USER': 'root',               
+        'PASSWORD': config("PASSWORD"),  
+        'HOST': '127.0.0.1',          
+        'PORT': '3306',               
     }
 }
 
